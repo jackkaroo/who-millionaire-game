13952a6f00d72424c940b8b139a361eec2f619a8
jackkaroo <kateryna.horulia@gmail.com>
2021-06-27 16:15:31 +0300

    return BrowserRouter ифсл

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import StartPage from './containers/StartPage';
 import PlayPage from './containers/PlayPage';
